d945cec1d895876f171cb68304d1130fe48b6ae5
Tyson Lupul <wavesrcool@icloud.com>
2022-09-07 21:28:07 -0500

    Update working tree.

---
+++ packages/flows-core/lib/database/postgresql/connection/FlowsCoreDatabasePostgreSQLConnection.d.ts
@@ -0,0 +1,7 @@
+import { TypesFiguresFlowsFunctionsDatabaseConnection } from "@wavesrcool/flows-functions";
+export declare class FlowsCoreDatabasePostgreSQLConnection {
+    private databaseConnection;
+    constructor(f: TypesFiguresFlowsFunctionsDatabaseConnection);
+    initialize(): Promise<void>;
+}
+//# sourceMappingURL=FlowsCoreDatabasePostgreSQLConnection.d.ts.map
